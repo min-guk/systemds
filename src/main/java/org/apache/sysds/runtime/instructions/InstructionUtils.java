99e2da1f86d13e9354df4b0f7a105dc29dca1840
min-guk <koreachoi96@gmail.com>
2026-01-15 00:58:41 +0100

    Always strip federated output flag for worker instructions

--- src/main/java/org/apache/sysds/runtime/instructions/InstructionUtils.java
+++ src/main/java/org/apache/sysds/runtime/instructions/InstructionUtils.java
@@ -1215,11 +1215,10 @@ public class InstructionUtils {
 	 * @return instruction string prepared for federated request
 	 */
 	public static String instructionStringFEDPrepare(String inst, CPOperand varOldOut, long id, CPOperand[] varOldIn, long[] varNewIn, boolean rmFederatedOutput){
-		boolean isFedInstr = inst.startsWith(ExecType.FED.name() + Lop.OPERAND_DELIMITOR);
 		String linst = replaceExecTypeWithCP(inst);
 		linst = replaceOutputOperand(linst, varOldOut, id);
 		linst = replaceInputOperand(linst, varOldIn, varNewIn);
-		if(rmFederatedOutput && isFedInstr)
+		if(rmFederatedOutput)
 			linst = removeFEDOutputFlag(linst);
 		return linst;
 	}
